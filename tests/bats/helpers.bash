3e59c238b8ee46d9f58362f42b428c81687d862e
Brice Burgess <nesta@iceburg.net>
2016-08-02 18:36:34 -0400

    introduce cat_fixture for dumping fixture contents

--- tests/bats/helpers.bash
+++ tests/bats/helpers.bash
@@ -3,5 +3,24 @@
 HELPERS_LOADED=true
 REPO_ROOT=${REPO_ROOT:-"$(git rev-parse --show-toplevel)"}
 
+# path to writable test target
 TMPDIR=$BATS_TMPDIR/dex-tests
 mkdir -p $TMPDIR
+
+#
+# runtime fns
+#
+
+error(){
+  printf "\033[31m%s\n\033[0m" "$@" >&2
+  exit 1
+}
+
+cat_fixture(){
+  local fixture=$BATS_TEST_DIRNAME/fixtures/$1
+  [ -e $fixture ] || fixture=$BATS_TEST_DIRNAME/../fixtures/$1
+  [ -e $fixture ] || error "unable to resolve fixture $1"
+
+  cat $fixture
+  return 0
+}
