5675267a826fe509606ef7cf2abf42d793b1cca1
Brice Burgess <nesta@iceburg.net>
2016-08-04 11:28:37 -0400

    add help and tests for all subcommands

---
+++ main_update.sh
@@ -0,0 +1,24 @@
+#
+# lib.d/main_update.sh for dex -*- shell-script -*-
+#
+
+main_update(){
+
+  local runstr="display_help"
+
+  if [ $# -eq 0 ]; then
+    display_help $CMD 2
+  else
+    while [ $# -ne 0 ]; do
+      case $1 in
+        -h|--help)         display_help ;;
+        *)                 unrecognized_arg "$1" ;;
+      esac
+      shift
+    done
+  fi
+
+  $runstr
+  exit $?
+  
+}
