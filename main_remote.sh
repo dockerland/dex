a8c8208786e2141b993764d9bfc1cb1fa0cfbfc5
Brice Burgess <nesta@iceburg.net>
2016-08-05 00:11:35 -0400

    add and test dex-setup routine

--- main_remote.sh
+++ main_remote.sh
@@ -4,13 +4,17 @@
 
 main_remote(){
 
-  local runstr="display_help"
+  local reset=false
+  local vars=()
+
+  local runstr="vars_print"
 
   if [ $# -eq 0 ]; then
     display_help 2
   else
     while [ $# -ne 0 ]; do
       case $1 in
+        ls)                dex-remote-ls ;;
         -h|--help)         display_help ;;
         *)                 unrecognized_arg "$1" ;;
       esac
@@ -20,5 +24,26 @@ main_remote(){
 
   $runstr
   exit $?
+}
 
+dex-remote-lookup(){
+  exit
+}
+
+
+dex-remote-add(){
+  exit
+}
+
+dex-remote-ls(){
+  exit
+}
+
+
+dex-remote-pull(){
+  exit
+}
+
+dex-remote-rm(){
+  exit
 }
