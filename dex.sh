e91c101b401e4842433188648bc1508d1139208d
Brice Burgess <nesta@iceburg.net>
2016-08-08 18:34:40 -0500

    minor improvements

--- dex.sh
+++ dex.sh
@@ -11,10 +11,12 @@ main(){
   CMD="main"
   ORIG_CMD="$0 $@"
 
-  # DEX_HOME: dex workspace : ~/.dex
+  # DEX_API: api version: v1
   # DEX_BIN_DIR: location where dex installs : /usr/local/bin
   # DEX_BIN_PREFIX: prefix of dexecutabls : d
+  # DEX_HOME: dex workspace : ~/.dex
   # DEX_NETWORK: enables network fetching : true
+  # DEX_TAG_PREFIX: tag used when running/building images : dex/v1
 
   DEX_VARS=( DEX_API DEX_BIN_DIR DEX_BIN_PREFIX DEX_HOME DEX_NETWORK DEX_TAG_PREFIX )
   vars_load ${DEX_VARS[@]}
