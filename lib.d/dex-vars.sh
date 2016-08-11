57c74269f08e0bd5e24048aa77ab2ae02651e228
Brice Burgess <nesta@iceburg.net>
2016-08-11 13:45:40 -0400

    group command fns into indivudal scripts
    the main_ fns are intended for argparsing + passing runtime

---
+++ lib.d/dex-vars.sh
@@ -0,0 +1,45 @@
+#
+# lib.d/dex-vars.sh for dex -*- shell-script -*-
+#
+
+dex-vars-print(){
+  while [ $# -ne 0 ]; do
+    eval "printf \"$1=\$$1\n\""
+    shift
+  done
+}
+
+
+dex-vars-reset(){
+  for var in "${DEX_VARS[@]}"; do
+    unset $var
+  done
+}
+
+
+dex-vars-init(){
+  while [ $# -ne 0 ]; do
+    case $1 in
+      DEX_API) eval "$1=\${$1:-v1}" ;;
+      DEX_BIN_DIR) eval "$1=\${$1:-/usr/local/bin}" ;;
+      DEX_BIN_PREFIX) eval "$1=\${$1:-d}" ;;
+      DEX_HOME) eval "$1=\${$1:-~/.dex}" ;;
+      DEX_NAMESPACE) eval "$1=\${$1:-dex/\$DEX_API}" ;;
+      DEX_NETWORK) eval "$1=\${$1:-true}" ;;
+      *) ERRCODE=127; error "$1 has no default configuration value" ;;
+    esac
+    shift
+  done
+}
+
+
+dex-vars-shellprint(){
+  # @TODO -- shell detection for fish|export
+  while [ $# -ne 0 ]; do
+    eval "printf \"export $1=\$$1\n\""
+    shift
+  done
+
+  printf "# Run this command to configure your shell: \n"
+  printf "# eval \$($ORIG_CMD)\n\n"
+}
--- lib.d/main_image.sh
+++ lib.d/main_image.sh
@@ -10,6 +10,7 @@ main_image(){
 
   local runstr="display_help"
   FORCE_FLAG=false
+  SKIP_NAMESPACE=false
   QUIET_FLAG=
 
   if [ $# -eq 0 ]; then
@@ -24,113 +25,15 @@ main_image(){
         -f|--force)       FORCE_FLAG=true ;;
         -h|--help)        display_help ;;
         -q|--quiet)       QUIET_FLAG="-q" ;;
-        --api-version)    arg_var "$2" DEX_API && shift ;;
+        -a|--all)         SKIP_NAMESPACE=true ;;
         *)                unrecognized_arg "$1" ;;
       esac
       shift
     done
   fi
 
-  dex-setup
+  dex-init
   $runstr
   exit $?
 
-}
-
-
-dex-image-build(){
-  # when installing, we prefix with "dex/$DEX_API-install"
-  local tag_prefix=${1:-$DEX_TAG_PREFIX}
-  local built_image=false
-
-  if [ -z "$LOOKUP" ]; then
-    ERRCODE=2
-    error "image-add requires an image name, package name, or wildcard match to install"
-  fi
-
-  dex-lookup-parse $LOOKUP || error "lookup failed to parse $1" 
-  for dockerfile in $(dex-lookup-dockerfiles); do
-    local image=$(basename $(dirname $dockerfile))
-    local tag="$tag_prefix/$image:$DEX_REMOTE_IMAGETAG"
-    log "building $tag ..."
-    (
-      set -e
-      cd $(dirname $dockerfile)
-      docker build -t $tag \
-        --label=dex-api=$DEX_API \
-        --label=dex-tag-prefix=$tag_prefix \
-        --label=dex-image=$image \
-        --label=dex-tag=$DEX_REMOTE_IMAGETAG \
-        --label=dex-remote=$DEX_REMOTE \
-        -f $(basename $dockerfile) .
-    ) && built_image=true
-  done
-
-  $built_image && {
-    log "built $DEX_REMOTE/$DEX_REMOTE_IMAGESTR"
-    exit 0
-  }
-
-  error "failed to find images matching $DEX_REMOTE/$DEX_REMOTE_IMAGESTR"
-}
-
-
-dex-image-ls(){
-  local tag_prefix=${1:-$DEX_TAG_PREFIX}
-  local filters="--filter=label=dex-tag-prefix=$tag_prefix"
-
-  if [ ! -z "$LOOKUP" ]; then
-    dex-lookup-parse $LOOKUP
-
-    [ ! "$DEX_REMOTE" = "*" ] && \
-      filters="$filters --filter=label=dex-remote=$DEX_REMOTE"
-
-    [ ! "$DEX_REMOTE_IMAGESTR" = "*" ] && \
-      filters="$filters --filter=label=dex-image=$DEX_REMOTE_IMAGESTR"
-
-    [ ! "$DEX_REMOTE_IMAGETAG" = "latest" ] && \
-      filters="$filters --filter=label=dex-tag=$DEX_REMOTE_IMAGETAG"
-  fi
-
-
-  docker images $QUIET_FLAG $filters
-}
-
-
-dex-image-rm(){
-  local tag_prefix=${1:-$DEX_TAG_PREFIX}
-
-  if [ -z "$LOOKUP" ]; then
-    ERRCODE=2
-    error "image-rm requires an image name, package name, or wildcard match to install"
-  fi
-
-  dex-lookup-parse $LOOKUP
-
-  local removed_image=false
-  local filters="--filter=label=dex-tag-prefix=$tag_prefix"
-  local force_flag=
-  $FORCE_FLAG && force_flag="--force"
-
-
-  [ ! "$DEX_REMOTE" = "*" ] && \
-    filters="$filters --filter=label=dex-remote=$DEX_REMOTE"
-
-  [ ! "$DEX_REMOTE_IMAGESTR" = "*" ] && \
-    filters="$filters --filter=label=dex-image=$DEX_REMOTE_IMAGESTR"
-
-  [ ! "$DEX_REMOTE_IMAGETAG" = "latest" ] && \
-    filters="$filters --filter=label=dex-tag=$DEX_REMOTE_IMAGETAG"
-
-  for image in $(docker images -q $filters); do
-    #@TODO stop running containers?
-    docker rmi $force_flag $image && removed_image=true
-  done
-
-  $removed_image && {
-    log "removed $DEX_REMOTE/$DEX_REMOTE_IMAGESTR"
-    exit 0
-  }
-
-  error "failed to remove any images matching $DEX_REMOTE/$DEX_REMOTE_IMAGESTR"
 }
