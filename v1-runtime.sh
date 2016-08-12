a9fb3a0f4b9b13df1ea616ca0e32da310e3f3346
Brice Burgess <nesta@iceburg.net>
2016-08-12 12:45:36 -0400

    user docker_ prefix for all labels

--- v1-runtime.sh
+++ v1-runtime.sh
@@ -3,20 +3,20 @@
 # runtime defaults,
 #  images may provide a org.dockerland.dex.<var> label supplying a value
 #  that overrides these default values, examples are:
-#  org.dockerland.dex.host_home
-#  org.dockerland.dex.host_workspace
+#  org.dockerland.dex.docker_home
+#  org.dockerland.dex.docker_workspace
 #  org.dockerland.dex.docker_flags
 
-__host_home=~
-__host_workspace=$(pwd)
+__docker_home=~
+__docker_workspace=$(pwd)
 __docker_flags=
 __docker_entypoint=
 __docker_cmd=
 
 # You may override these by exporting the following vars:
 #
-# DEX_HOST_HOME - directory that will be mounted to the container's $HOME
-# DEX_HOST_WORKSPACE - directory that be mounted as container's CWD
+# DEX_DOCKER_HOME - directory that will be mounted to the container's $HOME
+# DEX_DOCKER_WORKSPACE - directory that be mounted as container's CWD
 # DEX_DOCKER_FLAGS - flags passed to docker run
 # DEX_DOCKER_ENTRYPOINT - alternative entrypoint passed to docker run
 # DEX_DOCKER_CMD - alternative command passed to docker run
@@ -31,15 +31,15 @@ v1-runtime(){
 
   # augment defaults with image meta
   local prefix="org.dockerland.dex"
-  for label in host_home host_workspace docker_flags; do
+  for label in docker_home docker_workspace docker_flags; do
     # @TODO reduce this to a single docker inspect command
     val=$(docker inspect --format "{{ index .Config.Labels \"$prefix.$label\" }}" $__image)
     [ -z "$val" ] && continue
     eval "__$label=$val"
   done
 
-  DEX_HOST_HOME=${DEX_HOST_HOME:-$__host_home}
-  DEX_HOST_WORKSPACE=${DEX_HOST_WORKSPACE:-$__host_workspace}
+  DEX_DOCKER_HOME=${DEX_DOCKER_HOME:-$__docker_home}
+  DEX_DOCKER_WORKSPACE=${DEX_DOCKER_WORKSPACE:-$__docker_workspace}
   DEX_DOCKER_FLAGS=${DEX_DOCKER_FLAGS:-$__docker_flags}
   DEX_DOCKER_ENTRYPOINT=${DEX_DOCKER_ENTRYPOINT:-$__docker_entypoint}
   DEX_DOCKER_CMD=${DEX_DOCKER_CMD:-$__docker_cmd}
@@ -57,10 +57,10 @@ v1-runtime(){
     DEX_DOCKER_FLAGS="$DEX_DOCKER_FLAGS --interactive -e DEX_PIPED=true"
     __pipe="cat - |"
   fi
-  
+
   eval $__pipe docker run $DEX_DOCKER_FLAGS \
-    -v $DEX_HOST_HOME:/dex/home \
-    -v $DEX_HOST_WORKSPACE:/dex/workspace \
+    -v $DEX_DOCKER_HOME:/dex/home \
+    -v $DEX_DOCKER_WORKSPACE:/dex/workspace \
     -e HOME=/dex/home \
     -e DEX_API=$DEX_API \
     --rm --workdir=/dex/workspace -u $(id -u):$(id -g) --log-driver=none \
