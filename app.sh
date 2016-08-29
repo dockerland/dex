a7263d3fa5401fb763a3c6dc2071bbbe2f409474
Brice Burgess <nesta@iceburg.net>
2016-08-29 18:42:44 -0400

    de-activate docker-machine
    we should always build and run images against the local docker host.
    
    @TODO support boot2docker | "default" docker-machine

--- app.sh
+++ app.sh
@@ -1,3 +1,17 @@
+__local_docker(){
+  (
+    __deactivate_machine
+    exec docker "$@"
+  )
+}
+
+__deactivate_machine(){
+  # @TODO support boot2docker / concept of "default" machine
+  type docker-machine &>/dev/null && {
+    eval $(docker-machine env --unset --shell bash)
+  }
+}
+
 dex-ping(){
   echo "${1:-pong}"
   exit 0
