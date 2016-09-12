36fe8a8eb1c253d21c9748a7430a45256a330899
Brice Burgess <nesta@iceburg.net>
2016-09-12 13:05:47 -0400

    suppress network fetching noisiness

--- lib.d/helpers/network.sh
+++ lib.d/helpers/network.sh
@@ -7,18 +7,16 @@ fetch-url(){
   local WGET_PATH=${WGET_PATH:-wget}
   local CURL_PATH=${CURL_PATH:-curl}
 
-  if ( type $WGET_PATH >/dev/null 2>&1 ); then
-    $WGET_PATH $1 -O $2 || ( rm -rf $2 ; exit 1 ) 
-  elif ( type $CURL_PATH >/dev/null 2>&1 ); then
-    $CURL_PATH -Lfo $2 $1
+  if ( type $WGET_PATH &>/dev/null ); then
+    $WGET_PATH $1 -qO $2 || ( rm -rf $2 ; exit 1 )
+  elif ( type $CURL_PATH &>/dev/null ); then
+    $CURL_PATH -Lfso $2 $1
   else
-    log "failed to fetch $2 from $1" "missing curl and wget"
+    log "failed to fetch $2 from $1" "missing both curl and wget"
     return 2
   fi
 
-  [ $? -eq 0 ] && \
-    log "fetched fetch $2 from $1" && \
-    return 0
+  [ $? -eq 0 ] && return 0
 
   log "failed to fetch $2 from $1"
   return 126
