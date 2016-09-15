0a4ccb2d94bb51e64f4ddb1b20babc19a6da582b
Brice Burgess <nesta@iceburg.net>
2016-09-15 16:24:46 -0400

    deactive DOCKER_HOST even when missing docker-machine

--- helpers/docker.sh
+++ helpers/docker.sh
@@ -9,5 +9,8 @@ __deactivate_machine(){
   # @TODO support boot2docker / concept of "default" machine
   type docker-machine &>/dev/null && {
     eval $(docker-machine env --unset --shell bash)
+    return
   }
+  # lets be safe and unset if missing docker-machine
+  unset DOCKER_HOST DOCKER_TLS_VERIFY DOCKER_CERT_PATH DOCKER_MACHINE_NAME
 }
