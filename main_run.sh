23959a4eb95be2efaaf687d434ce0a4161bc032b
Brice Burgess <nesta@iceburg.net>
2016-10-18 16:41:53 -0400

    respect -- termination in argparsing

--- main_run.sh
+++ main_run.sh
@@ -15,7 +15,10 @@ main_run(){
   if [ $# -eq 0 ]; then
     display_help 2
   else
-    set -- $(normalize_flags "" "$@")
+    echo $@
+    set -- $(normalize_flags_first "" "$@")
+    echo $@
+    exit
     while [ $# -ne 0 ]; do
       case $1 in
         -b|--build)     __build_flag=true ;;
