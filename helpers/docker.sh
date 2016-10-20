fc1b9d9baa8401f54c2ebd3907b235eeaf6539f2
Brice Burgess <nesta@iceburg.net>
2016-10-20 12:48:46 -0500

    add docker_safe_name helper fn

--- helpers/docker.sh
+++ helpers/docker.sh
@@ -14,3 +14,9 @@ __deactivate_machine(){
   # lets be safe and unset if missing docker-machine
   unset DOCKER_HOST DOCKER_TLS_VERIFY DOCKER_CERT_PATH DOCKER_MACHINE_NAME
 }
+
+docker_safe_name(){
+  local name="$@"
+  set -- "${name:0:1}" "${name:1}"
+  printf "%s%s" "${1//[^a-zA-Z0-9]/0}" "${2//[^a-zA-Z0-9_.-]/_}"
+}
