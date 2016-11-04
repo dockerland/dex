349d938bcabdc6fd0e408cc081e754f3ac259317
Chris Potter <chris.potter@blueacorn.com>
2016-11-04 15:13:55 -0400

    local docker compose

--- lib.d/helpers/docker.sh
+++ lib.d/helpers/docker.sh
@@ -5,6 +5,13 @@ __local_docker(){
   )
 }
 
+__local_docker_compose(){
+  (
+    __deactivate_machine
+    exec docker-compose "$@"
+  )
+}
+
 __deactivate_machine(){
   # @TODO support boot2docker / concept of "default" machine
   type docker-machine &>/dev/null && {
