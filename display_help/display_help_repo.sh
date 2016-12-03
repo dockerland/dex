0856fcca66b81922b1490b481b478df4b09cc77c
Brice Burgess <nesta@iceburg.net>
2016-12-03 11:48:40 -0500

    implement dex repo

---
+++ display_help/display_help_repo.sh
@@ -0,0 +1,55 @@
+display_help_repo(){
+  cat <<-EOF
+
+dex - run applications without installing them or their dependencies.
+
+About repo:
+  dex images are built from Dockerfiles in git repositories.
+
+  Repositories are defined one-per-line in \$DEX_HOME/sources.list in a
+  space-delimited "<name> <url>" format -- and this command manages them.
+
+  Repositories are cloned to \$DEX_HOME/checkouts when added, and refreshed when
+  the --pull flag is passed to dex run|install, or explicitly via dex repo pull.
+
+  Removing or adding repositories will not effect installed images, allowing
+  dexecutables to function as normal. Use the "dex image" command to
+  manage images built from sources.
+
+Usage:
+  dex repo [options...] <command>
+
+Options:
+  -h|--help
+    Displays help
+
+Commands:
+  add [-f|--force] <name> <url>
+    Add a source repository. <name> must be alphanumeric, and <url> can be
+    a git remote or local path. Forcing overwrites existing <name>.
+    Examples:
+      dex repo add extra git@github.com:dockerland/dex-dockerfiles-extra.git
+      dex repo add local /path/to/my/repo
+
+  ls [-d|--defaults] [name(s)...]
+    Print repositories from \$DEX_HOME/sources.list. Passing name(s) limits the
+    listing. Passing defaults prints the $DEX_RUNTIME default sources.list.
+
+  pull [-f|--force] [name(s)...]
+    Pull (refresh) named repositories, or all repositories if no name passed.
+    Forcing overwrites any working copy changes in \$DEX_HOME/checkouts/<name>
+    Examples:
+      dex repo pull
+      dex repo pull local extra
+
+  rm [-f|--force] <name(s)...>
+    Remove named source repositories. Forcing disregards working copy changes.
+    Examples:
+      dex repo rm local --force
+
+  reset [-f|--force]
+    Reset sources.list to $DEX_RUNTIME default. Forcing supresses prompts and
+    enables networking.
+
+EOF
+}
---
+++ repo.sh
@@ -0,0 +1,125 @@
+main_repo(){
+  local operand
+  local list=()
+  __defaults=false
+  __force=false
+
+  [ $# -eq 0 ] && display_help 1
+  set -- $(args/normalize_flags_first "" "$@")
+  while [ $# -ne 0 ]; do
+    case "$1" in
+      -h|--help)
+        display_help  ;;
+      -d|--defaults)
+        __defaults=true ;;
+      -f|--force)
+        __force=true ;;
+      --)
+        shift ; list=( "$@" ) ; break ;;
+      -*)
+        args/unknown "$1" "flag" ;;
+      add|ls|pull|reset|rm)
+        operand="dex/repo-$1" ;;
+      *)
+        [ -z "$operand" ] && args/unknown "$1"
+        list+=( "$1" )
+        ;;
+    esac
+    shift
+  done
+
+  # seed sources list if it's missing
+  [ -e "$__sources" ]  || (
+    __force=true
+    dex/repo-reset
+    dex/repo-pull
+  )
+
+  shell/execfn "$operand" "${list[@]}"
+}
+
+dex/repo-add(){
+  local repo="$1"
+  local url="$2"
+  [[ -z "$repo" || -z "$url" ]] && die "please provide a repo name and url"
+
+  dex/repo-exists $repo && {
+    io/confirm "$repo already exists. overwrite?" || return 1
+    __force=true dex/repo-rm "$repo"
+  }
+
+  io/notice "adding \e[1m$repo\e[21m ..."
+  file/interpolate "$__sources" "^$repo " "$repo $url"
+  dex/repo-pull "$repo" || {
+    __force=true dex/repo-rm "$repo"
+    die/exception "failed to add $repo"
+  }
+  io/success "added \e[1m$repo\e[21m"
+}
+
+dex/repo-exists(){
+  [ -n "$(dex/repo-ls $1)" ]
+}
+
+dex/repo-reset(){
+  io/warn "reseting $__sources"
+  network/fetch "$__sources_url" "$__sources"
+  io/success "reset $__sources"
+}
+
+dex/repo-ls(){
+  local name
+  local url
+  local junk
+  local filter="$@"
+  local format="${__format:-\$name \$url}"
+  local cmd="cat $__sources"
+  $__defaults && cmd="network/print $__sources_url"
+
+  $cmd | while read name url junk ; do
+
+    # skip blank, malformed, or comment lines
+    [[ -z "$name" || -z "$url" || "#" = "$name" ]] && continue
+
+    # skip lines not matching our filter
+    [[ ${#@} -gt 0 && " $name " != *" $filter "* ]] && continue
+
+    eval "printf \"$format\n\""
+  done
+}
+
+dex/repo-pull(){
+  local repo
+  local url
+  local path
+
+  # we use fd9 to allow for nested reads/prompts
+  while read -u9 repo url ; do
+    io/log "pulling $repo repository..."
+    path="$__checkouts/$repo"
+    if [ -d "$path" ]; then
+      git/pull "$path" || return 1
+    else
+      git/clone "$url" "$path" || return 1
+    fi
+    io/log "pulled $repo"
+  done 9< <(dex/repo-ls "$@")
+}
+
+dex/repo-rm(){
+  local repo
+  local path
+
+  # we use fd3 to allow for nested reads/prompts
+  for repo in $(__format="\$name" dex/repo-ls "$@"); do
+    if ! $__force; then
+      io/confirm "remove \e[1m$repo\e[21m from $__sources ?" || continue
+    fi
+    file/sed_inplace "$__sources" "/^$repo /d"
+    io/log "removing $repo from $__sources"
+
+    path="$__checkouts/$repo"
+    prepare/overwrite "$path" "remove checkout $path ?" && \
+      io/log "removed checkout $path"
+  done
+}
